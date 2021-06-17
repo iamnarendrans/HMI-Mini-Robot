092c064cb8450a0bdf8c63a42cdd555fa2f49114
Narendran Srinivasan <67863890+iamnarendrans@users.noreply.github.com>
2021-06-17 09:59:21 -0400

    Add files via upload

---
+++ esp32_tank.ino
@@ -0,0 +1,192 @@
+#include <math.h>
+#include <WiFi.h>
+const char* ssid     = "enter your SSID";
+const char* password = "enter your Password";
+WiFiServer server(80);
+String header;
+String valueString = String(5);
+int pos1 = 0;
+int pos2 = 0;
+
+//define pins
+#define EN34 33
+#define O4A 25
+#define O3A 26
+#define EN12 27
+#define O2A 14
+#define O1A 12
+
+#define pwmFreq 100
+#define pwmLeftChannel 2
+#define pwmRightChannel 1
+#define pwmRes 8
+
+
+int rx, ry = 0; //recieved coordinates
+int leftspeed = 0;
+int rightspeed = 0;
+
+unsigned long lastMillis = 0;
+
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(O4A, OUTPUT);
+  pinMode(O3A, OUTPUT);
+  pinMode(O2A, OUTPUT);
+  pinMode(O1A, OUTPUT);
+
+  //PWM Setup
+  ledcSetup(pwmRightChannel, pwmFreq, pwmRes);
+  ledcSetup(pwmLeftChannel, pwmFreq, pwmRes);
+  ledcAttachPin(EN34, pwmLeftChannel);
+  ledcAttachPin(EN12, pwmRightChannel);
+
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+  WiFiClient client = server.available();   // Listen for incoming clients
+}
+
+void loop() {
+  client = server.available();
+  if (client) {
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        header += c;
+
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            printWebsite();
+
+            if (header.indexOf("GET /?M") >= 0) {
+              valueString = header.substring(header.indexOf("/?X") + 3, header.indexOf("/X"));
+              rx = valueString.toInt();
+              //              Serial.print("X ");
+              //              Serial.print(valueString);
+
+              valueString = header.substring(header.indexOf("/?Y") + 3, header.indexOf("/Y"));
+              ry = valueString.toInt() * -1 ; //the joystick considers down to be positive so we adjust that here
+              //              Serial.print("   Y ");
+              //              Serial.print(valueString);
+              //
+              //              Serial.print("            ");
+              //              Serial.println(millis() - lastMillis);
+              //              lastMillis = millis();
+
+            }
+            client.println();
+            setMotorsXY(rx, ry);
+
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+  }
+}
+
+void setMotorsXY(int x, int y) {
+  //lets move things to polar, it will make the motor setting much easier since we really only have to look at theta
+  double theta = PI / 2; //default to forward
+  double rightSpeed = 0;
+  double leftSpeed = 0;
+  if (x != 0) {  //just some exception handling here
+    if (y > 0) {
+      theta =  PI - atan2(y, x * (-1));
+    } else if (y < 0) {
+      theta = 2 * PI + atan2(y, x);
+    }
+  } else {
+    if (y > 0) {
+      theta = PI / 2;
+    } else {
+      theta = 3 * PI / 2;
+    }
+  }
+  double r = sqrt(x * x + y * y) / 100; //we want this value to be less than 1 for later oprations
+
+  if (theta < PI / 2) {
+    leftSpeed = 100 * r;
+    rightSpeed = (200 * (2 * theta) / PI - 100) * r;
+  } else if (theta < PI) {
+    leftSpeed = (100 - 400 * ((theta / PI) - .5)) * r;
+    rightSpeed = 100 * r;
+  } else if (theta < 3 * PI / 2) {
+    leftSpeed = -100 * r;
+    rightSpeed = (100 - 400 * ((theta / PI) - 1)) * r;
+  } else if (theta < 2 * PI) {
+    rightSpeed = -100 * r;
+    leftSpeed = ( 400 * ((theta / PI) - 1.5) - 100) * r;
+  }
+
+  setMotors((int)rightSpeed, (int) leftSpeed);
+
+}
+
+
+/*dumb switching function, intended to be used with other timing code
+  takes values from -100 to 100, with 0 being off
+*/
+void setMotors(int _right, int _left) {
+  int rightVal = abs(_right) ;
+  int leftVal = abs(_left);
+  //
+  Serial.print("Right ");
+  Serial.print(_right);
+  Serial.print(" Left ");
+  Serial.println(_left);
+
+  if (_right > 0) {
+    ledcWrite(pwmRightChannel, map(rightVal, 0, 100, 0, 245));
+    digitalWrite(O2A, HIGH);
+    digitalWrite(O1A, LOW);
+  } else if (_right < 0) {
+    ledcWrite(pwmRightChannel, map(rightVal, 0, 100, 0, 245));
+    digitalWrite(O2A, LOW);
+    digitalWrite(O1A, HIGH);
+  } else {
+    ledcWrite(pwmRightChannel, map(rightVal, 0, 100, 0, 245)); //it'll only get here if its zero anyway
+    digitalWrite(O2A, LOW);
+    digitalWrite(O1A, LOW);
+  }
+
+  if (_left > 0) {
+    ledcWrite(pwmLeftChannel, map(leftVal, 0, 100, 0, 245));
+    digitalWrite(O4A, LOW);
+    digitalWrite(O3A, HIGH);
+  } else if (_left < 0) {
+    ledcWrite(pwmLeftChannel, map(leftVal, 0, 100, 0, 245));
+    digitalWrite(O4A, HIGH);
+    digitalWrite(O3A, LOW);
+  }
+  else {
+    ledcWrite(pwmLeftChannel, map(leftVal, 0, 100, 0, 245));
+    digitalWrite(O3A, LOW);
+    digitalWrite(O4A, LOW);
+  }
+}
+
---
+++ print_website.ino
@@ -0,0 +1,28 @@
+void printWebsite(){
+client.println("<html><head><meta charset=\"utf-8\">");
+client.println("<meta name=\"viewport\" content=\"width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0\">");
+client.println("<script src = \"https://cdn.jsdelivr.net/gh/jeromeetienne/virtualjoystick.js/virtualjoystick.js\"></script>");
+client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
+client.println("<style>");
+client.println("body {overflow  : hidden;background-color: #BBB;}");
+client.println("#container {width: 100%;height: 100%;overflow: hidden;padding:0;margin:0;-webkit-user-select:none;-moz-user-select:none;}");
+client.println("</style><title> ESP32 Bot Control Site </title></head><body>");
+client.println("<div id=\"container\"><h1>ESP32 Bot Control</h1></div><div id=\"info\"><span id=\"result\"></span></div> <script>");
+client.println("console.log(\"touchscreen is\", VirtualJoystick.touchScreenAvailable() ? \"available\" : \"not available\");");
+client.println("var joystick  = new VirtualJoystick({");
+client.println("container : document.getElementById('container'),");
+client.println("mouseSupport  : true,");
+client.println("limitStickTravel : true,");
+client.println("stickRadius : 100");
+client.println("});");
+client.println("joystick.addEventListener('touchStart', function(){");
+client.println("console.log('down')");
+client.println("})");
+client.println("joystick.addEventListener('touchEnd', function(){");
+client.println("console.log('up')");
+client.println("})");
+client.println("$.ajaxSetup({timeout:35});"); //the most important line
+client.println("setInterval(function(){");
+client.println("var message = \"/?M/?X\"+Math.round(joystick.deltaX())+\"/X/?Y\"+Math.round(joystick.deltaY())+\"/Y/M\";$.get(message);var outputEl = document.getElementById('result');}, 75);");
+client.println("</script></body></html>");
+};
